f2828b54112332610f0f4f984149b8ebddbc851c
bondur <tanya_bondar_03@mail.ru>
2021-12-14 19:53:10 +0300

    Create lab 1_17.cpp

---
+++ lab 1/lab 1_17.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+int main()
+{
+    std::cout << "Enter a number: "; // просим пользователя ввести любое число
+    int x = 0;
+    std::cin >> x;
+    int k;
+if (x < 100) {
+    printf("число x однозначное или двузначное\n");
+}
+else {
+    k = x / 10 / 10 % 10;
+    printf("k = %d\n", k);
+    }
+}
