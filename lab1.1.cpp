f969e6cbab8b14f423c4bf1fc5f60d05f263f63c
bondur <tanya_bondar_03@mail.ru>
2021-12-14 19:01:47 +0300

    Delete lab1.1.cpp

--- lab1.1.cpp
+++
@@ -1,9 +0,0 @@
-﻿//1E6; 0xffffffL; 0731UL; 123456789LU; -5; +0xaf; OX0 -правильные ответы
-#include <iostream>
-int main()
-{
-	int a;
-	a = 123456789LU; //вместо 123456789LU проверяем все числа по порядку
-	printf("%d", a);
-	return 0;
-}
