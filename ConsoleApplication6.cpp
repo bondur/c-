8aa6b2971df8852c3f0c95f85ec041479a878b42
bondur <tanya_bondar_03@mail.ru>
2021-12-14 16:34:19 +0300

    Create ConsoleApplication6.cpp

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdafx.h>
+#include <iostream>
+using namespace std;
+
+int _tmain()
+{
+	double b = 2.345034;
+	double c = b - (int)b;
+	if (c > 0)
+	{
+		cout << "Число не целое";
+	}
+	else
+	{
+		cout << "Число целое";
+	}
+}
