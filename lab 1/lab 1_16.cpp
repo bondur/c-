f6054fbff26817c1b4f224aa016d4c58f270c98d
bondur <tanya_bondar_03@mail.ru>
2021-12-14 19:43:41 +0300

    Create lab 1_16.cpp

---
+++ lab 1/lab 1_16.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x = 3.2, A = 0;
+    A = (2 * (pow(x, 4))) - (3 * pow(x, 3)) + (4 * pow(x, 2)) - (5 * x) + 6;
+    printf("A = %f", A);
+    return 0;
+}
