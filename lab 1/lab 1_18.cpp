f34289cfb2b4dce8aae46ee69e28a5a543284445
bondur <tanya_bondar_03@mail.ru>
2021-12-14 21:07:16 +0300

    Create lab 1_18.cpp

---
+++ lab 1/lab 1_18.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    for (int i; i < 3; i++) {
+        k += x % 10;
+        x /= 10;
+    }
+    cout << k << endl;
+}
