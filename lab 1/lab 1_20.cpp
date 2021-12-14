1906028371d10638d47e30ab8dd8bfacaef44037
bondur <tanya_bondar_03@mail.ru>
2021-12-14 21:32:51 +0300

    Create lab 1_20.cpp

---
+++ lab 1/lab 1_20.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a number: "; // просим пользователя ввести любое число
+    int x = 0;
+    std::cin >> x;
+    int k;
+    k += x % 10;
+    x /= 10;
+    for (int i; i < 2; i++)
+    {
+        k = k * 10 + x % 10;
+        x /= 10;
+    }
+    std::cout << k << std::endl;
+}
