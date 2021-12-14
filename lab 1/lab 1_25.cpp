62572dffd66f19ed724489118a6c738d1c79d40e
bondur <tanya_bondar_03@mail.ru>
2021-12-14 21:35:40 +0300

    Create lab 1_25.cpp

---
+++ lab 1/lab 1_25.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int c = 0;
+    unsigned char x, y, z, u;
+    cin >> x >> y >> z >> u;
+    c = x;
+    c = c << 8 | y;
+    c = c << 8 | z;
+    c = c << 8 | u;
+    printf("\n%d(%x), %d(%x), %d(%x), %d(%x)", x, x, y, y, z, z, u, u);
+    printf("\n%d(%x)", c, c);
+
+    return 0;
+}
