7d616a6935c8b6b5107b2a146a176a479ed5ab16
bondur <tanya_bondar_03@mail.ru>
2021-12-14 19:15:27 +0300

    Create lab 1.6.cpp

---
+++ lab 1/lab 1.6.cpp
@@ -0,0 +1,11 @@
+﻿//a). E1 op= E2		и		E1 = E1 op E2 - выражение не эквивалентны
+#include <iostream>
+int main() 
+{
+	int a, b, c;
+	b = 6, c = 5;
+	//c*=2+b;
+	c = c * (2 + b);
+	printf("%d", c);
+	return 0;
+}
