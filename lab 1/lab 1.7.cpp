cdaf0bfd66a387028bc4aadfb19bc11c5a7f317b
bondur <tanya_bondar_03@mail.ru>
2021-12-14 19:19:46 +0300

    Create lab 1.7.cpp

---
+++ lab 1/lab 1.7.cpp
@@ -0,0 +1,10 @@
+﻿// вычисление значений выражений
+#include <iostream>
+int main() 
+{
+    int a, b, c, res;   a = 2;  b = 6;  c = 3;
+    //    res = -  -  - a;
+    //    res = a --- b;
+    printf("res = %d", a);
+    return 0;
+}
