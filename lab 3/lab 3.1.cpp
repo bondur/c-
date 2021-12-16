2cc5ce60057a24b35ffdab24719ce82fecd0e7f4
bondur <tanya_bondar_03@mail.ru>
2021-12-16 20:30:57 +0300

    Create lab 3.1.cpp

---
+++ lab 3/lab 3.1.cpp
@@ -0,0 +1,18 @@
+﻿//Подсчитать количество натуральных чисел n(111  n  999), в записи которых есть
+//а) две одинаковые цифры;
+#include <iostream>
+int main()
+{
+	int counter = 0;
+	for (int n = 111; n <= 999; i++) {
+		if ((n % 10 == n / 10 % 10) || (n % 10 == n / 100 % 10) || (n / 10 % 10 == n / 100 % 10) counter += 1;
+	}
+}
+/*b) только две одинаковые цифры.
+int counter = 0;
+for (int n = 111, tempCounter = 0; n <= 999; i++) {
+	tempCounter += (n % 10 == n / 10 % 10) ? 1 : 0;
+	tempCounter += (n / 10 % 10 == n / 100 % 10) ? 1 : 0;
+	tempCounter += (n % 10 == n / 100 % 10) ? 1 : 0;
+	if (tempCounter == 2) counter++;
+} */
