dceff0c8bb6e827565364dd2e7632b15daeb4a2c
bondur <tanya_bondar_03@mail.ru>
2021-12-16 19:30:33 +0300

    Create lab 1_10.cpp

---
+++ lab 2/lab 1_10.cpp
@@ -0,0 +1,16 @@
+﻿//Выбрать наибольшее из трёх заданных чисел.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	if ((a > b) && (a > c))
+		cout << "Наибольшее число: " << a;
+	if ((b > a) && (b > c))
+		cout << "Наибольшее число: " << b;
+	if ((c > a) && (c > b))
+		cout << "Наибольшее число: " << c;
+}
