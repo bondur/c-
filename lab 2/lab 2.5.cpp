6900463295eb5d1f2f6718d5004928c352402cd6
bondur <tanya_bondar_03@mail.ru>
2021-12-16 18:53:40 +0300

    Create lab 2.5.cpp

---
+++ lab 2/lab 2.5.cpp
@@ -0,0 +1,37 @@
+﻿/*
+Какие ошибки содержаться в приведенных ниже фрагментах программ:
+	a) Ошибка: case x>0
+	int x;
+	….
+	switch (x)
+		{
+		  case 5: x++; break;
+		  case x>0: x--; break;
+		  default: x+=66;
+	}
+	b) верно
+	int x;
+	….
+	switch (x)
+		{
+		  case 5: x+=7; break;
+		  case 6: case 11: --x; break;
+	}
+
+	c) верно
+	int x;
+	….
+	switch (x)
+		{
+		  case 5: x*=16;
+		  case 6: case 11: x-=23; break;
+	}
+	d) Ошибка: switch(5)
+	int x;
+	….
+	switch (5)
+		{
+		  case 5: x+=7; break;
+		  case 6: case 11: --x; break;
+	}
+*/
