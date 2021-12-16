2ed54a3bc2f4faa4ec616cd0661ad77693ec1c8f
bondur <tanya_bondar_03@mail.ru>
2021-12-16 19:21:56 +0300

    Create lab 2.7.cpp

---
+++ lab 2/lab 2.7.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main()
+{
+	int A, B, C;
+	std::cin >> A >> B >> C;
+	std::cout << "A=" << A << " B=" << B << " C=" << C << "\n";
+	if ((A % 2 == 0) || (B % 2 == 0) || (C % 2 == 0))
+		printf("Есть хотя бы одно четное чило");
+	else
+		printf("нет четных чисел");
+}
