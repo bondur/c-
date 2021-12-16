1b77242482bc5b0413837d1b30a7839c70679960
bondur <tanya_bondar_03@mail.ru>
2021-12-16 20:31:05 +0300

    Create lab 3.2.cpp

---
+++ lab 3/lab 3.2.cpp
@@ -0,0 +1,11 @@
+﻿//Подсчитать количество натуральных чисел n(102  n  987), в которых все три цифры различны.
+#include <iostream>
+int main()
+{
+	for (int I = 102; i <= 987; i++) {
+		tempCounter += (n % 10 == n / 10 % 10) ? 1 : 0;
+		tempCounter += (n / 10 % 10 == n / 100 % 10) ? 1 : 0;
+		tempCounter += (n % 10 == n / 100 % 10) ? 1 : 0;
+		if (tempCounter == 0) counter++;
+	}
+}
