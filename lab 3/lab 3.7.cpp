a85c8d22c09cd333db991d952ed7944a0b2e8a5c
bondur <tanya_bondar_03@mail.ru>
2021-12-16 21:08:16 +0300

    Create lab 3.7.cpp

---
+++ lab 3/lab 3.7.cpp
@@ -0,0 +1,13 @@
+﻿//Для данного вещественного положительного числа a найти наименьшее натуральное положительное n такое, что 1 + 1 / 2 + 1 / 3 + ... + 1 / n > a.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = I, I = 1;
+	double sum = 0;
+		while(sum <= a) {
+		sum += (double)1 / I;
+		I++;
+	}
+	cout << sum;
+}
