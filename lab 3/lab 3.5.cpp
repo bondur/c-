e434ee9a0ed6b23a99d6a85fe000693318ad9718
bondur <tanya_bondar_03@mail.ru>
2021-12-16 20:44:41 +0300

    Create lab 3.5.cpp

---
+++ lab 3/lab 3.5.cpp
@@ -0,0 +1,14 @@
+﻿//Определить, верно ли, что куб суммы цифр натурального числа n равен n2.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int sum = 0, n = 123, sqrN = n * n;
+	while(n) {
+		sum += n % 10;
+		N/= 10;
+	}
+	if (sum * sum * sum == sqrN) cout << “Куб суммы равен n в квадрате” << endl;
+	
+	else cout << “Куб суммы не равен n в квадрате” << endl;
+}
