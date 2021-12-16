3cfb651c4c0b79715df2f9edab2790d2e1a13d9e
bondur <tanya_bondar_03@mail.ru>
2021-12-16 20:54:02 +0300

    Create lab 3.6.cpp

---
+++ lab 3/lab 3.6.cpp
@@ -0,0 +1,13 @@
+﻿//Определить, является ли натуральное число n степенью числа 3.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 123;
+	double temp = n;
+	while (temp > 3) {
+		temp /= 3
+	}
+	if (temp == 3) cout << "Натуральное число является степенью 3" << endl;
+	else cout << "Натуральное число не является степенью 3" << endl;
+}
