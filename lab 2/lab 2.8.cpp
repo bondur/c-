8ba0430c39e801ef226780b9620fb338a1c0c455
bondur <tanya_bondar_03@mail.ru>
2021-12-16 19:22:00 +0300

    Create lab 2.8.cpp

---
+++ lab 2/lab 2.8.cpp
@@ -0,0 +1,18 @@
+﻿//Даны три числа.Вывести на экран те из них, которые принадлежат заданному отрезку[e, f].
+#include <iostream>
+int main()
+{
+	int a, b, c, e, f;
+	std::cin >> a >> b >> c;
+	std::cin >> e >> f;
+	if (a < f && a > e) {
+		std::cout << a << " ";
+	}
+	if (b < f && b > e) {
+		std::cout << b << " ";
+	}
+	if (c < f && c > e) {
+		std::cout << c << " ";
+	}
+	return 0;
+}
