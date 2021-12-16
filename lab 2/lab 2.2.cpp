8319999260856c5d5fd7366becdbd4aec1e0558d
bondur <tanya_bondar_03@mail.ru>
2021-12-16 18:53:24 +0300

    Create lab 2.2.cpp

---
+++ lab 2/lab 2.2.cpp
@@ -0,0 +1,8 @@
+﻿/*
+Эквивалентны ли следующие фрагменты программы:
+if (e1) if (e2) S1; else S2; неверно
+if (e1) { if (e2) S1; else S2; } неверно
+if (e1) { if (e2) S1; } else S2; верно
+if (e1)  if (e2) S1; else ; else S2; верно
+if (e1)  if (e2) S1; else S2; else ; неверно
+*/
